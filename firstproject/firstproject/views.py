c40b40eea9a53d3282ce902702e40454d9ea3f34
kanchan-pat <82234347+kanchan-pat@users.noreply.github.com>
2022-07-20 08:57:35 -0400

    Comments - Kanchan 07/20

--- firstproject/firstproject/views.py
+++ firstproject/firstproject/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 #from django.views.generic.list import ListView
 
+# I see you have been wondering what to render for home page. Generally it's the landing page for any web application. You can have a classbased view inheritaed from base class 'TemplateView'
+# which basically takes a template_name that will have a welcome message and all the navigation links for web application 
+
+
 #class HomePage(ListView):
 #    model = Admission
 #    #querySet = Admission.objects.all()
