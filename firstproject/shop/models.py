6ac2eacc94c77f107f854c7bcc1c47871a254b57
kanchan-pat <82234347+kanchan-pat@users.noreply.github.com>
2022-07-20 08:58:51 -0400

    Comments - Kanchan 07/20

--- firstproject/shop/models.py
+++ firstproject/shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# great! Can we have more attributes related to a product along with image? 
 class Product(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=40)
